8a56262a1a2d6d241018437be3f5337b8c690f5c
Max Katkalov <makskatkalov@gmail.com>
2023-10-20 23:11:30 +0300

    add station_app and station_user to installed_apps

--- station_servise/settings.py
+++ station_servise/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "station_app.apps.StationAppConfig",
+    "station_user.apps.StationUserConfig",
 ]
 
 MIDDLEWARE = [
