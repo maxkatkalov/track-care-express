45bd034b16daf270b5f0fc4c77d716dfb13945bb
Max Katkalov <makskatkalov@gmail.com>
2023-10-21 19:55:48 +0300

    documentation implement
    - Swagger UI with drf_spectacular

--- station_servise/settings.py
+++ station_servise/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_spectacular",
     "django_extensions",
     "station_app.apps.StationAppConfig",
     "station_user.apps.StationUserConfig",
@@ -127,3 +128,20 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Cinema API',
+    'DESCRIPTION': 'It is the best cinema API',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "defaultModelRendering": "model",
+        "defaultModelsExpandDepth": 2,
+        "defaultModelExpandDepth": 2,
+    }
+}
