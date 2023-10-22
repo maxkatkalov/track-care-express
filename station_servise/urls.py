c45db34d9008b10eef6c04225cd4ec146c3c2059
Max Katkalov <makskatkalov@gmail.com>
2023-10-22 14:14:06 +0300

    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) to main urls

--- station_servise/urls.py
+++ station_servise/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
@@ -9,4 +11,4 @@ urlpatterns = [
     path("api/doc/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/doc/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path("api/station-user/", include("station_user.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
